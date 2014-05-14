b15a6128ccd306055e651ed729a30f5ccf76bbde
Jim Chambers <mrhistory@gmail.com>
2014-05-14 18:15:26 -0400

    Created Organization class.

--- lib/organization-api/organization.rb
+++ lib/organization-api/organization.rb
@@ -1,7 +1,33 @@
 module OrganizationAPI
 
   class Organization
-    
+    attr_accessor :name, :address1, :address2, :city, :state, :zipcode, :phone_number, :admins, :applications
+
+    def initialize(params)
+      @name = params[:name] ||= nil
+      @address1 = params[:address1] ||= nil
+      @address2 = params[:address2] ||= nil
+      @city = params[:city] ||= nil
+      @state = params[:state] ||= nil
+      @zipcode = params[:zipcode] ||= nil
+      @phone_number = params[:phone_number] ||= nil
+      @admins = params[:admins] ||= nil
+      @applications = params[:applications] ||= nil
+    end
+
+    def to_json
+      {
+        name: @name,
+        address1: @address1,
+        address2: @address2,
+        city: @city,
+        state: @state,
+        zipcode: @zipcode,
+        phone_number: @phone_number,
+        admins: @admins,
+        applications: @applications
+      }.to_json
+    end
   end
 
 end
--- spec/organization-api_spec.rb
+++ spec/organization-api_spec.rb
@@ -7,4 +7,23 @@ describe OrganizationAPI do
       'web_service_user',
       'messingaroundinboats')
   end
+
+  before(:each) do
+    @org1 = OrganizationAPI::Organization.new( {
+        name: 'Org1',
+        address1: '123 Fake St.',
+        city: 'Lawrenceville',
+        state: 'Ga',
+        zipcode: '30044',
+        phone_number: '1234567890'
+      })
+    @org2 = OrganizationAPI::Organization.new( {
+        name: 'Org2',
+        address1: '456 Fake St.',
+        city: 'Lawrenceville',
+        state: 'Ga',
+        zipcode: '30044',
+        phone_number: '3216540987'
+      })
+  end
 end
